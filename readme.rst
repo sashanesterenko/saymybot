14bacf8447e0dd0f70cef22edbefabbfb910ee7d
Sasha Nesterenko <nesterenko.sasha@gmail.com>
2018-12-11 22:53:32 +0300

    readme, code improvements

---
+++ readme.rst
@@ -0,0 +1,33 @@
+saymybot
+========
+
+Saymybot is a produc of following Learn Python's Bot Path. It is awkward but it works.
+
+Installation
+------------
+
+Install virtual enviroment and activate it. After that run:
+.. code-block:text
+    pip install -r requirements.txt
+
+Put some derp pictures in /images folder. Name them derp*.jp*g
+
+Setup
+-----
+
+Create file settings.py and add following to it:
+
+..code-block:python
+    PROXY = {'proxy_url': 'socks5://YOUR-SOCKS-PROXY:1080',
+    'urllib3_proxy_kwargs': {'username': 'learn', 'password': 'python'}}
+
+    API = "API-KEY-BOTFATHER-GAVE-YOU"
+
+    USER_EMOJI = [':smiley_cat:', ':smiling_imp:', ':panda_face:', ':dog:']
+
+Running
+-------
+
+In active virtual enviroment run:
+..code-block: text
+    python3 saymybot.py
--- saymybot.py
+++ saymybot.py
@@ -1,82 +1,14 @@
 
-from glob import glob
 
-from datetime import datetime
-import ephem
-import logging
-from random import choice
 
-from emoji import emojize
-from telegram import ReplyKeyboardMarkup, KeyboardButton
 from telegram.ext import Updater, CommandHandler, MessageHandler, RegexHandler, Filters
+from handlers import *
+
 
 import settings
 
-logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
-                    level=logging.INFO,
-                    filename='bot.log'
-                    )
 
 
-def greet_user(bot, update, user_data):
-    emo = get_user_emo(user_data)
-    user_data['emo'] = emo
-    text = 'Hello, {}'.format(emo)
-    contact_button = KeyboardButton('Send contact', request_contact=True)
-    location_button = KeyboardButton('Send location', request_location=True)
-    my_keyboard = ReplyKeyboardMarkup([['Send derp','Change emoji'],
-        [contact_button, location_button]])
-    update.message.reply_text(text, reply_markup=get_keyboard())
-
-
-def talk_to_me(bot, update, user_data):
-    emo = get_user_emo(user_data)
-    user_text = "Hi {} {}! You wrote: {}".format(update.message.chat.first_name, emo, update.message.text)
-    logging.info('User: %s, Chat id: %s, Message: %s', update.message.chat.username, update.message.chat.id,
-                update.message.text)
-    update.message.reply_text(user_text, reply_markup=get_keyboard())
-
-def change_emo(bot, update, user_data):
-    if 'emo' in user_data:
-        del user_data['emo']
-    emo = get_user_emo(user_data)
-    update.message.reply_text('Here you go, {}'.format(emo), reply_markup=get_keyboard())
-
-def todays_constellation(bot, update, user_data):
-    if len(update.message.text.split()) == 2:
-        command, planet_name = update.message.text.split()
-    now = datetime.now()
-    planet_class = getattr(ephem, planet_name, None) #вернёт ephem.Mars()
-    if planet_class:
-        planet = planet_class(now) #planet = ephem.Mars(now)
-        constellation = ephem.constellation(planet)
-        update.message.reply_text(constellation, reply_markup=get_keyboard())
-
-def send_derp_picture(bot, update, user_data):
-    derp_list = glob('images/derp*.jp*g')
-    derp_pic = choice(derp_list)
-    bot.send_photo(chat_id=update.message.chat.id, photo=open(derp_pic, 'rb'), reply_markup=get_keyboard())
-
-def get_user_emo(user_data):
-    if 'emo' in user_data:
-        return user_data['emo']
-    else:
-        user_data['emo'] = emojize(choice(settings.USER_EMOJI), use_aliases=True)
-        return user_data['emo']   
-
-def get_keyboard():
-    contact_button = KeyboardButton('Send contact', request_contact=True)
-    location_button = KeyboardButton('Send location', request_location=True)
-    my_keyboard = ReplyKeyboardMarkup([['Send derp','Change emoji'],
-        [contact_button, location_button]], resize_keyboard=True)
-
-def get_contact(bot, update, user_data):
-    print(update.message.contact)
-    update.message.reply_text('Here you go: {}'.format(get_user_emo(user_data)), reply_markup=get_keyboard())
-
-def get_location(bot, update, user_data):
-    print(update.message.location)
-    update.message.reply_text('Here you go: {}'.format(get_user_emo(user_data)), reply_markup=get_keyboard())
 
 def main():
     mybot = Updater (settings.API, request_kwargs=settings.PROXY)
@@ -93,5 +25,5 @@ def main():
     mybot.start_polling()
     mybot.idle()
 
-
-main()
+if __name__ == "__main__":
+    main()
